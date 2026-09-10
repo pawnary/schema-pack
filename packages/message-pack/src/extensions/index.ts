5368166fdffe886e50186fa36e3dbc962261beae
ivyhjk <12478859+ivyhjk@users.noreply.github.com>
2026-09-10 20:40:14 -0300

    fix: export extensions interfaces types in barrel file

--- packages/message-pack/src/extensions/index.ts
+++ packages/message-pack/src/extensions/index.ts
@@ -2,3 +2,4 @@ export * from './timestampDate/index.ts';
 export type { default as MessagePackExtension } from './interfaces/messagePackExtension.ts';
 export * from './error/index.ts';
 export * from './serializer/index.ts';
+export type * from './interfaces/types.ts';
