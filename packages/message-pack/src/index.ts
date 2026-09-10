1d5885eeb916d7706917603efb7f85e8e1fa37aa
ivyhjk <12478859+ivyhjk@users.noreply.github.com>
2026-09-10 20:43:18 -0300

    feat: Add error and serializer extensions types to main barrel file

--- packages/message-pack/src/index.ts
+++ packages/message-pack/src/index.ts
@@ -1,19 +1,19 @@
 export {
-  Encoder,
   type EncoderOptions,
   type MessagePackEncoder,
   type MessagePackTextEncoder,
   type ExtensionEncoder,
+  Encoder,
   DefaultTextEncoder,
   NodeTextEncoder,
   encode,
 } from './encoder/index.ts';
 
 export {
-  Decoder,
-  DefaultTextDecoder,
   type MessagePackDecoder,
   type MessagePackTextDecoder,
+  Decoder,
+  DefaultTextDecoder,
   NodeTextDecoder,
   decode,
 } from './decoder/index.ts';
@@ -21,6 +21,9 @@ export {
 export {
   type MessagePackExtension,
   type ErrorExtensionOptions,
+  type ErrorExtensionFactory,
+  type ErrorExtensionFactoryInput,
+  type Constructor,
   ErrorExtension,
   TimestampDateExtension,
   SerializerExtension,
