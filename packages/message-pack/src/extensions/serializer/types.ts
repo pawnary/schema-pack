b68c095f46b81b50d41f52dafd912455df098bc1
ivyhjk <12478859+ivyhjk@users.noreply.github.com>
2026-09-10 20:37:18 -0300

    feat: improve base serializer extension types

--- packages/message-pack/src/extensions/serializer/types.ts
+++ packages/message-pack/src/extensions/serializer/types.ts
@@ -1,14 +1,11 @@
 import type { Constructor } from '../interfaces/types.ts';
 
-export type SerializerExtensionFactory<
-  TValue extends object,
-  TInput = unknown,
-> = (input: TInput) => InstanceType<Constructor<TValue>>;
+export type SerializerExtensionFactory<TValue extends object, TInput> = (
+  input: TInput,
+) => InstanceType<Constructor<TValue>>;
 
 export type SerializerExtensionFactories<
-  TValue extends object = object,
-  TInput = unknown,
-> = [
-  Constructor<TValue>,
-  SerializerExtensionFactory<InstanceType<Constructor<TValue>>, TInput>,
-][];
+  TValue extends object,
+  TInput,
+  TFactory extends SerializerExtensionFactory<TValue, TInput>,
+> = [Constructor<TValue>, TFactory][];
