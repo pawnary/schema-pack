478b597709580c24aebed0c3617b8b07f92bd7f1
ivyhjk <12478859+ivyhjk@users.noreply.github.com>
2026-09-10 20:38:08 -0300

    fix: remove unnessary ConstructorExtensionFactory type

--- packages/message-pack/src/extensions/interfaces/types.ts
+++ packages/message-pack/src/extensions/interfaces/types.ts
@@ -2,7 +2,3 @@ export type Constructor<TValue> = abstract new (
   // oxlint-disable-next-line typescript/no-explicit-any
   ...args: any[]
 ) => TValue;
-
-// export type ConstructorExtensionFactory<TValue extends object, TInput> = (
-//   input: TInput,
-// ) => InstanceType<Constructor<TValue>>;
